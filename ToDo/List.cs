df0b0d2b1edd5befd0853f61abcc67aa3f1ff844
Cavit Bagci <cavit.bagci04@gmail.com>
2024-02-18 19:48:26 +0300

    List methods added.

---
+++ ToDo/List.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    internal class List
+    {
+        public string Baslik { get; set; }
+        public string Icerik { get; set; }
+        public string AtananKisi { get; set; }
+        //public Dictionary<int, string> TakimUyeleri { get; set; }
+        public List(string baslik)
+        {
+            Baslik = baslik;
+        }
+        public List(string baslik, string icerik, string atananKisi)
+        {
+            //Console.Write("Başlık : ");
+            Baslik = baslik;
+            //Console.WriteLine();
+            //Console.Write("İçerik : ");
+            Icerik = icerik;
+            //Console.WriteLine();
+            //Console.Write("Atanan Kişi : ");
+            AtananKisi = atananKisi;
+            //TakimUyeleri = TakimArkadaslari.TakimUyeleri;
+
+        }
+    }
+
+    internal enum Boyut
+    {
+        xs = 0,
+        s = 1,
+        m = 2,
+        l = 3,
+        xl = 4
+    }
+
+    internal class TakimArkadaslari
+    {
+        public static Dictionary<int, string> TakimUyeleri = new Dictionary<int, string>
+        {
+            {1, "Ahmet"},
+            {2, "Mehmet"},
+            {3, "Ayşe"},
+            {4, "Fatma"}
+        };
+    }
+}
--- ToDo/SelectPage.cs
+++ ToDo/SelectPage.cs
@@ -8,16 +8,47 @@ namespace ToDo
 {
     internal class SelectPage
     {
+        private List<List> lists;
+        public void Liste()
+        {
+            lists = new List<List>()
+            {
+                new List("deneme","denemeiçerik","a"),
+                new List("deneme2","denemeiçerik2","b")
+            };
+        }
+
+        private List<List> lists2;
+        private void InProgressLine()
+        {
+            lists2 = new List<List>()
+            {
+                new List("deneme3","denemeiçerik","a"),
+                new List("deneme4","denemeiçerik2","b")
+            };
+        }
+
+        private List<List> lists3;
+        private void DoneLine()
+        {
+            lists3 = new List<List>()
+            {
+               new List("Başlık")
+            };
+        }
         internal void Main()
         {
             Console.WriteLine("Hangi işlemi yapmak istediğinizi seçiniz.");
             Console.WriteLine("(1) Board Listelemek \n(2) Board'a Kart Eklemek \n(3) Board'dan Kart Silmek \n(4) Kart Taşımak");
-            
+
             int secim = int.Parse(Console.ReadLine());
+            Liste();
+            InProgressLine();
+            DoneLine();
             switch (secim)
             {
                 case 1:
-                    Console.WriteLine();
+                    ListBoard();
                     break;
                 case 2:
                     Console.WriteLine();
@@ -28,10 +59,52 @@ namespace ToDo
                 case 4:
                     Console.WriteLine();
                     break;
-                    default:
+                default:
                     Console.WriteLine("Geçersiz değer girdiniz lütfen 1-4 arasında bir değer giriniz.");
                     break;
             }
+        }
+
+        private void ListBoard()
+        {
+            Console.WriteLine("TODO Line");
+            Console.WriteLine("************************");
+            foreach (List list in lists)
+            {
+                Console.WriteLine("Başlık : {0} \nİçerik : {1} \nAtanan Kişi : {2}", list.Baslik, list.Icerik, list.AtananKisi);
+                Console.WriteLine("");
+            }
+            Console.WriteLine("IN PROGRESS Line");
+            Console.WriteLine("************************");
+            foreach (List list in lists2)
+            {
+                Console.WriteLine("Başlık : {0} \nİçerik : {1} \nAtanan Kişi : {2}", list.Baslik, list.Icerik, list.AtananKisi);
+                Console.WriteLine("");
+            }
+            Console.WriteLine("DONE Line");
+            Console.WriteLine("************************");
+            foreach(List list in lists3)
+            {
+                Console.WriteLine(list.Baslik);
+            }
+            /*TODO Line
+            ************************
+            Başlık      :
+            İçerik      :
+            Atanan Kişi :
+            Büyüklük    :
+ 
+            IN PROGRESS Line
+            ************************
+            Başlık      :
+            İçerik      :
+            Atanan Kişi :
+            Büyüklük    :
+
+            DONE Line
+            ************************
+            ~ BOŞ ~
+            */
         }
     }
 
